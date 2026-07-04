3849955535e0cedf0eabab72f6f520bd99c143ce
quantumsmarts-1415 <quantumsmarts@gmail.com>
2026-07-04 13:09:30 -0600

    Add new presets to the presets array

--- src/presets/index.js
+++ src/presets/index.js
@@ -6,11 +6,22 @@ import { nestedBoxes } from './nestedBoxes.js'
 import { phoneStand } from './phoneStand.js'
 import { phoneCase } from './phoneCase.js'
 import { dragon } from './dragon.js'
+import { cableClip } from './cableClip.js'
+import { headphoneStand } from './headphoneStand.js'
+import { adjustableDock } from './adjustableDock.js'
+import { penCupOrganizer } from './penCupOrganizer.js'
+import { chargerStand } from './chargerStand.js'
+import { pegHook } from './pegHook.js'
+import { drillBitOrganizer } from './drillBitOrganizer.js'
+import { toolRackSlots } from './toolRackSlots.js'
+import { stackableBin } from './stackableBin.js'
+import { screwdriverHolderBlock } from './screwdriverHolderBlock.js'
 
-export const presets = [boxWithLid, nestedBoxes, phoneStand, phoneCase, dragon]
+export const presets = [boxWithLid, nestedBoxes, phoneStand, phoneCase, dragon, cableClip, headphoneStand, adjustableDock, penCupOrganizer, chargerStand, pegHook, drillBitOrganizer, toolRackSlots, stackableBin, screwdriverHolderBlock]
 
 export function defaultParams(preset) {
+
   const out = {}
-  for (const p of preset.params) out[p.key] = p.default
-  return out
+      for (const p of preset.params) out[p.key] = p.default
+    return out
 }
